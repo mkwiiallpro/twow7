acbc1f22aaea01a083288c95ba9ed2dd37527b10
Michael Lyga <31606955+mkwiiallpro@users.noreply.github.com>
2024-09-06 08:31:58 -0400

    thursday commit
    I forgor :3

--- onebyone/main.cpp
+++ onebyone/main.cpp
@@ -8,6 +8,15 @@
 #include <ctime>
 #include <algorithm>
 #include <cassert>
+std::vector<std::string> csv_handler(const std::string &s){
+    if(s.length() ==0) return NULL;
+    std::vector<std::string> result;
+    std::string buffer;
+    for(char c: s){
+
+    }
+    return result;
+}
 int main(int argc, char** argv){
     std::cout<<"Hello World"<<std::endl;
     std::cout<<"The number of arguments is "<<argc<<std::endl;
@@ -18,10 +27,20 @@ int main(int argc, char** argv){
        - DRPs and Dummies are not supported at this time.
        TODO: Integrate this all with a vote parser 
      */
-    std::ifstream f1("results.csv"); 
     std::vector<std::string> names;
     std::vector<std::string> responses;
     std::vector<double> means;
     std::vector<double> stdev;
+    std::ifstream f1("results.csv"); 
+    std::string s;
+    while(std::getline(f1,s)){
+        // changes windows strings into unix-style strings
+            if(s[s.length()-1]=='\r'){
+            s=s.substr(0,s.length()-1);
+        }
+    }
+    f1.close();
+
+    
     return 0;
 }
